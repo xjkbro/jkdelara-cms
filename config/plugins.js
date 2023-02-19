bdbfca9a7e1601b593e6df1370ea9c0bd40113b7
Jason-Kyle De Lara <jason8oh8@gmail.com>
2023-02-18 22:34:33 -0800

    graphql

--- config/plugins.js
+++ config/plugins.js
@@ -23,7 +23,7 @@ module.exports = ({ env }) => ({
       endpoint: "/graphql",
       shadowCRUD: true,
       playgroundAlways: false,
-      depthLimit: 7,
+      depthLimit: 10,
       amountLimit: 100,
       apolloServer: {
         tracing: false,
